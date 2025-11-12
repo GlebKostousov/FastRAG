816d22578fd3eba32656226d4a5519bce1214960
GlebKostousov <kostousjr@gmail.com>
2025-11-12 14:59:17 +0300

    refactor(parser-lib): setup module import

--- FastRag/document_processor/parsers/__init__.py
+++ FastRag/document_processor/parsers/__init__.py
@@ -1,1 +1,11 @@
 """Module for parse all type off input file"""
+
+from document_processor.parsers.code_parser import CodeParser
+from document_processor.parsers.html_parser import HTMLParser
+from document_processor.parsers.pdf_parser import PDFParser
+
+__all__ = (
+    "CodeParser",
+    "HTMLParser",
+    "PDFParser",
+)
